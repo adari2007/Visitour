e3eb5ec93bbb6e4cbeb75c4886fd200b516fb79e
Developer <dev@example.com>
2026-05-04 23:59:02 -0500

    Fix the issues

---
+++ apps/mobile/babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './src',
+          },
+          extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.js', '.jsx', '.json'],
+        },
+      ],
+    ],
+  };
+};
+
